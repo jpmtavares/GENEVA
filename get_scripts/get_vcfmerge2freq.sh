1f20c30645b002ae29328bf3f13ea4e3b05838bd
Joana Tavares <jpmtav@gmail.com>
2018-12-17 12:13:01 +0000

    Update get_vcfmerge2freq.sh
    1) stop script when some command fails
    2) add help function with parameters using getopts
    3) sort final file by chromosome position

--- get_scripts/get_vcfmerge2freq.sh
+++ get_scripts/get_vcfmerge2freq.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-
+set -e 
+set -o pipefail
 #######################################################################################
 #                                                                                     #
 #           Merge samples VCF files and get inHouse frequency table                   #
@@ -7,16 +8,55 @@
 #######################################################################################
 
 ############################################################
-#   SETUP
+#   HELP function
 ############################################################
+usage="$(basename "$0") [-h] [-v <path to VCF files>] [-d <path to output>] [-o <output name>] [-p <number of process to run>]
 
-# Provide a variable with the location of vcf files
-if [ $# -eq 0 ] #check if there are any argument provided
-  then
-    vcfPath=$(pwd)
-  else
-    vcfPath="$1"
-fi
+       -- program to merge VCF samples, and calculate allele frequencies among those samples --
+
+where:
+    -h    show this help text
+    -v    [default: current directory] set directory where VCF samples are stored
+    -d    [default: same as <vcf>] set directory where allele frequency table is written
+    -o    [default: inHouse_freq_] set output name for allele frequency file
+    -p    [default: 24] set number of process to run at the same time"
+
+##__________ SETUP __________##
+# Get the running date of script
+todaydate="$(date +%Y%m%d)"
+# Set current directory as default
+vcfPath=$(pwd)
+# Set output directory the same as <vcf>
+freqPath=${vcfPath}
+# Set outputname
+outputname="inHouse_freq_"
+# Set number of process
+processes=24
+
+while getopts ':h:v:d:o:p:' option; do
+  case "$option" in
+    h) echo "$usage"
+       exit
+       ;;
+    v) vcfPath=$OPTARG
+       ;;
+    d) freqPath=$OPTARG
+       ;;
+    o) outputname=$OPTARG
+       ;;
+    p) processes=$OPTARG
+       ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+  esac
+done
+shift $((OPTIND - 1))
 
 ############################################################
 #   1) merge vcf files with vcf-merge tool
@@ -24,12 +64,10 @@ fi
 echo "STEP 1:"
 echo "Merging the vcf files ..."
 
-# Get the running date of script
-todaydate="$(date +%Y%m%d)"
-
 echo "number of samples: "
 #Count number of sample
 ls ${vcfPath}/[0-9]*.vcf.gz | wc -l
+
 # Run vcf-merge and compress file with bgzip
 vcf-merge $(readlink -f ${vcfPath}/[0-9]*.vcf.gz) | bgzip -c > ${vcfPath}/todas_${todaydate}.vcf.gz
 # Create tabix index file
@@ -60,16 +98,17 @@ echo "STEP 3:"
 echo "Construct the table with the number of samples, number of homozigotics and the allelic frequences ..."
 #Get the number os samples, the homozigotics and the allelic frequences
 #The number of processes runing at the same time is set to 24 (-P option)
-find ${vcfPath}/chr*_*vcf.gz | xargs -n1 -P24 -I {} get_inHouseFreq.py -vcf {} -outname {}tmp.freq
-
-#exit script
-exit 1
+find ${vcfPath}/chr*_*vcf.gz | xargs -n1 -P${processes} -I {} get_inHouseFreq.py -vcf {} -outname {}tmp.freq
 
 #Create a unique file with the frequences
 cat ${vcfPath}/*tmp.freq >> ${vcfPath}/in_HouseInput_${todaydate}.txt
 
+#Sort file by chromosome position
+sort -k1,1 -k2,2n ${vcfPath}/in_HouseInput_${todaydate}.txt > ${freqPath}/${outputname}${todaydate}.txt
+
 #Remove all temporary files
 rm ${vcfPath}/chr*_*vcf.gz*
 rm ${vcfPath}/*tmp.freq
+rm ${vcfPath}/in_HouseInput_${todaydate}.txt
 
-echo "Finish!"
+echo "Finish!" 
