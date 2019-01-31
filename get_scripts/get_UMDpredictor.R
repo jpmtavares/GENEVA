8193c9f86103436d3a549e4de7fe3fdd7409b856
Joana Tavares <jpmtav@gmail.com>
2019-01-31 16:02:53 +0000

    Update get_UMDpredictor.R
    works with list of data.frames using parallelization; writes individual results for each transcript and concatenates them in a final file

--- get_scripts/get_UMDpredictor.R
+++ get_scripts/get_UMDpredictor.R
@@ -12,6 +12,7 @@ library(readr)
 library(stringr)
 library(biomaRt)
 library(dplyr)
+library(parallel)
 #______________________________________________
 # HELP function
 #______________________________________________
@@ -51,12 +52,12 @@ UMDpredictor<-function(ENSTranscripts){
   url<-paste("http://umd-predictor.eu/transcript_query2.php?name=",
              str_extract(ENSTranscripts,"(?<=0{5}).*$"),sep="")
   # read trnascript information
-  umd<-lapply(url, function(x) {
+  umd<-mclapply(url, function(x) {
     tryCatch({
       print(paste("Get ENST00000", str_extract(x,"\\d{6}"), " transcript.", sep=""))
       read.delim(x, header = F)},
       error=function(e){NULL})
-  })
+  }, mc.preschedule = TRUE, mc.cores = 8)
   
   # remove error entries
   print(paste("Couldn't get results for", ENSTranscripts[sapply(umd, is.null)], "transcript.", sep=" "))
@@ -64,11 +65,9 @@ UMDpredictor<-function(ENSTranscripts){
   umd<-umd[!sapply(umd, is.null)]
   
   # add ENSTranscript column
-  umd<-mapply(cbind, "ENSTranscript"=ENSTranscripts, umd,  SIMPLIFY=F)
-  # list 2 data.frame
-  umdpredictor<-do.call("rbind", umd)
-  names(umdpredictor)[-1]<-c("TranscriptPosition","Position","HGVS_c","HGVSp",
-                             "score","UMD-predictor")
+  umd<-mcmapply(cbind, "ENSTranscript"=ENSTranscripts, umd,  SIMPLIFY=F, mc.preschedule = TRUE, mc.cores = 8)
+  umd<-mclapply(umd, setNames, c("ENSTranscript","TranscriptPosition","Position","HGVS_c","HGVSp",
+                                 "score","UMD-predictor"), mc.preschedule = TRUE, mc.cores = 8)
   #________________________________
   # Get info from Ensembl biomart
   #________________________________
@@ -81,17 +80,23 @@ UMDpredictor<-function(ENSTranscripts){
   names(Ensembl_info)<-c("Chr", "HGNC_symbol", "ENSTranscript")
   print("BioMart retrieved.")
   
-  # get REF and ALT alleles
-  alleles<-do.call(rbind.data.frame, str_extract_all(as.character(umdpredictor$HGVS_c), "[ACGT]"))
   # join Ensembl and UMD-predictor
   print("Joining results...")
-  result<-left_join(umdpredictor, Ensembl_info)
-  result<-data.frame(Chr=result$Chr, Position=result$Position, Ref=alleles[,1], Alt=alleles[,2],
-                     HGVS_c=result$HGVS_c, HGVS_p=result$HGVSp, HGNC_symbol=result$HGNC_symbol,
-                     ENSTranscript=result$ENSTranscript, UMD_pred=result$`UMD-predictor`,
-                     UMD_score=result$score)
+  result<-mclapply(umd, left_join, Ensembl_info, mc.preschedule = TRUE, mc.cores = 8)
+  #________________________________
+  # Get final table
+  #________________________________
+  # get REF and ALT alleles and combine everything into a final table
+  final<-mclapply(result, function(x){
+    alleles<-do.call(rbind.data.frame, str_extract_all(as.character(x$HGVS_c), "[ACGT]"))
+    names(alleles)<-c("Ref","Alt")
+    return(data.frame(Chr=x$Chr, Position=x$Position, Ref=alleles$Ref, Alt=alleles$Alt,
+                      HGVS_c=x$HGVS_c, HGVS_p=x$HGVSp, HGNC_symbol=x$HGNC_symbol,
+                      ENSTranscript=x$ENSTranscript, UMD_pred=x$`UMD-predictor`,
+                      UMD_score=x$score))
+  }, mc.preschedule = TRUE, mc.cores = 8)
   
-  return(result)
+  return(final)
 }
 
 #________________________________________________________
@@ -129,13 +134,20 @@ umd<-UMDpredictor(transcripts)
 #########################################################
 # 3) write-table
 #########################################################
-print("Writing final table to file...")
-write.table(umd, paste(filename,".txt",sep=""), col.names=F, row.names=F, quote=F, sep="\t")
+print("Writing individual transcripts files...")
+dir.create("./UMD_tmp/", showWarnings = TRUE, recursive = FALSE)
+mclapply(names(umd), function(x){
+  write.table(umd[[x]], paste("./UMD_tmp/", x,".txt",sep=""), col.names=F, row.names=F, quote=F, sep="\t")
+}, mc.preschedule = TRUE, mc.cores = 8)
+
+print("Concatenate transcript files...")
+system(paste("cat ./UMD_tmp/*.txt > ", filename, ".txt",sep=""))
 sink() # close log file
 
 #########################################################
 # 4) sort otuput file and indexed it
 #########################################################
+print("Sort and Index file.")
 system(paste("sort -k1,1 -k2,2n ", paste(filename,".txt",sep=""), ">", paste(filename, "_sort.txt", sep=""), sep=" "))
 # index "_sort.txt"
 system(paste("bgzip", paste(filename, "_sort.txt", sep=""), sep=" "))
