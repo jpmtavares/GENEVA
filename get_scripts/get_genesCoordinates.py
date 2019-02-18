a461192cb29ac5586667e8bce24a5a1ec455980d
Brígida de Meireles <brigidameireles@gmail.com>
2019-02-18 17:51:34 +0000

    Change the script to read gzipped files

--- get_scripts/get_genesCoordinates.py
+++ get_scripts/get_genesCoordinates.py
@@ -1,4 +1,5 @@
 import argparse
+import gzip
 
 parser = argparse.ArgumentParser(
     description='Get the genes position, first and last position')
@@ -11,21 +12,22 @@ def createFileWithGenes(infile, outname):
     dic_genes = {}
     outfile=open(outname, "w")
 
-    with open(infile) as bed:
+    with gzip.open(infile, "rb") as bed:
+        next(bed)
         for line in bed:
             aux = line.rstrip().split("\t")
             id = aux[5]
             if "_" not in aux[0]:
                 if id in dic_genes.keys():
                     info = dic_genes.get(id, " ")
-                    dic_genes[id] = [info[0], info[1], aux[2]]
+                    dic_genes[id] = [info[0], info[1], aux[2], info[3]]
                 else:
-                    dic_genes[aux[5]] = [aux[0], aux[1], aux[2]]
+                    dic_genes[aux[5]] = [aux[0], aux[1], aux[2], aux[6]]
 
 
-    outfile.write("#Chro"+"\t"+"Start"+"\t"+"End"+"\t"+"Gene"+"\n")
+    outfile.write("#Chro"+"\t"+"Start"+"\t"+"End"+"\t"+"Gene"+"\t"+"Gene_alt"+"\n")
     for key, values in dic_genes.items():
-        outfile.write(values[0]+"\t"+values[1]+"\t"+values[2]+"\t"+key+"\n")
+        outfile.write(values[0]+"\t"+values[1]+"\t"+values[2]+"\t"+key+"\t"+values[3]+"\n")
 
     outfile.close()
     bed.close()
@@ -34,4 +36,4 @@ if __name__ == "__main__":
     f_genes = args.refseq
     outname = args.out_file
 
-    createFileWithGenes(f_genes, outname)
+createFileWithGenes(f_genes, outname)
