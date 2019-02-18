3a1b851a39e2c3fb92d2693793dbfc3a4a7cb6d9
Joana Tavares <jpmtav@gmail.com>
2019-02-18 14:38:44 +0000

    Update and rename create_RefSeq.R to create_RefSeqBED.R
    This scripts accepts the new RefSeq annotation file

--- create_scripts/create_RefSeqBED.R
+++ create_scripts/create_RefSeqBED.R
@@ -10,7 +10,10 @@
 # libraries
 #______________________________________________
 library(stringr)
+library(plyr)
 library(dplyr)
+library(magrittr)
+library(ape)
 #______________________________________________
 # HELP function
 #______________________________________________
@@ -32,24 +35,25 @@ rm(argsL)
 # Default setting when no all arguments passed or help needed
 if("--help" %in% args | is.null(args$exons) | is.null(args$introns) | is.null(args$clinical)) {
   cat("
-      create_refSeq.R [--help] [--exons==<path to UCSC BED file with exons>] [--introns==<path to UCSC BED file with introns>] [--clinical==<path to file with clinical transcripts>]
+      create_RefSeqBED.R [--help] [--exons==<path to UCSC BED file with exons>] [--introns==<path to UCSC BED file with introns>] [--RefSeq==<path to file with custom RefSeq annotation>]
       
       -- program to create BED file with RefSeq clinical transcripts along with ranking for exons and introns --
       
       where:
       --exons                      path to UCSC BED file with exons
       --introns                    path to UCSC BED file with introns
-      --clinical                   path to file with clinical transcripts\n\n")
+      --RefSeq                     path to file with custom RefSeq annotation\n\n")
   
   q(save="no")
 }
 
 # SETUP
 #________________________________________________________
-exons<-read.delim(args[["exons"]], header=F)
-introns<-read.delim(args[["introns"]], header=F)
-clinical<-read.delim(args[["clinical"]], header = TRUE)
-filename<-"refSeq_clinical"
+exons<-read.delim(args[["exons"]], header= F)
+introns<-read.delim(args[["introns"]], header= F)
+RefSeq<-read.delim(args[["RefSeq"]], header = TRUE)
+filename<-"RefSeqGRCh37"
+filename_clinical<-"RefSeqGRCh37_clinical"
 
 ################################################################
 # 1) join exons and introns
@@ -64,7 +68,7 @@ genes<-data.frame(genes[,c("Chr", "Start", "End")],
                   str_split_fixed(genes$Info, "_", 8),
                   "Strand"=genes[,c("Strand")]) %>%
   mutate(Transcripts=paste(X1, X2, sep="_")) %>%
-  select(Chr, Start, End, Transcripts, X3, X4, Strand)
+  dplyr::select(Chr, Start, End, Transcripts, X3, X4, Strand)
 
 names(genes)<-c("Chr", "Start", "End", "Transcripts", "Region", "Rank", "Strand")
 
@@ -82,26 +86,45 @@ bed<-genes %>%
          Chr=gsub("chr","", Chr)) %>% # remove "chr" 
   ungroup() %>%
   mutate(Region=paste(Region, Rank, "/", Total, sep="")) %>%
-  mutate(refSeq_mRNA=str_extract(Transcripts, "[^\\.]*")) %>%
-  inner_join(., clinical) %>%
+  mutate(refSeq_mRNA_noVersion=str_extract(Transcripts, "[^\\.]*")) %>%
+  inner_join(., RefSeq) %>%
   arrange(Chr, Start, End) %>%
-  select(Chr, Start, End, Region, Strand, HGNC_symbol, ENSGene, ENSTranscript, refSeq_mRNA, refSeq_protein)
+  dplyr::select(Chr, Start, End, Region, Strand, HGNC_symbol, HGNC_alternative_symbol, ENSGene, ENSTranscript, refSeq_mRNA, refSeq_protein,
+         refSeq_mRNA_noVersion, refSeq_protein_noVersion, LRG_id, clinical_transcript)
+
+# get only clinical transcripts
+bed_clinical<-bed %>%
+  filter(clinical_transcript=="yes") %>%
+  dplyr::select(-clinical_transcript)
 
 # turn-off scientific notation
 bed$Start <-format(bed$Start, scientific = FALSE, trim=T) 
 bed$End <-format(bed$End, scientific = FALSE, trim=T) 
 
+# turn-off scientific notation
+bed_clinical$Start <-format(bed_clinical$Start, scientific = FALSE, trim=T) 
+bed_clinical$End <-format(bed_clinical$End, scientific = FALSE, trim=T) 
 ################################################################
 # 4) write-table
 ################################################################
-write.table(bed, paste(filename, ".bed", sep=""), col.names=F, row.names=F, quote=F, sep="\t")
+dir.create("RefSeq_annotation/", showWarnings = TRUE, recursive = FALSE)
+
+write.table(bed, paste("RefSeq_annotation/", filename, ".bed", sep=""), col.names=F, row.names=F, quote=F, sep="\t")
+write.table(bed_clinical, paste("RefSeq_annotation/", filename_clinical, ".bed", sep=""), col.names=F, row.names=F, quote=F, sep="\t")
 
 ################################################################
-# 5) sort BED file, create BED for coverage analysis and indexed it
+# 6) sort BED file, create BED for coverage analysis and indexed it
 ################################################################
-system(paste("sort -k1,1 -k2,2n -k3,3n", paste(filename, ".bed", sep=""), ">", paste(filename, "_sort.bed", sep=""), sep=" "))
-# BED for coverage analysis
-system(paste("awk 'NF{NF-=1};1'", paste(filename, "_sort.bed", sep=""), "| uniq | awk '{ print $1\"\t\"$2\"\t\"$3\"\t\"$6\",\"$4\",\"$5\",\"$7\",\"$8\",\"$9}' | sed -r 's/\\s+/\\t/g' >", paste(filename, "_coverage.bed", sep=""), sep=" "))
+#system(paste("sort -k1,1 -k2,2n -k3,3n", paste("RefSeq_annotation/", filename, ".bed", sep=""), ">", paste("RefSeq_annotation/", filename, "_sort.bed", sep=""), sep=" "))
+system(paste("sort -k1,1 -k2,2n -k3,3n", paste("RefSeq_annotation/", filename_clinical, ".bed", sep=""), ">", paste("RefSeq_annotation/", filename_clinical, "_sort.bed", sep=""), sep=" "))
+
+# BED for coverage analysis: Chr    Start    End    Gene,Region,Strand,ENSGene,ENSTranscript,RefSeqNM
+#system(paste("awk 'NF{NF-=1};1'", paste("RefSeq_annotation/", filename, "_sort.bed", sep=""), "| uniq | awk '{ print $1\"\t\"$2\"\t\"$3\"\t\"$6\",\"$4\",\"$5\",\"$8\",\"$9\",\"$10}' | sed -r 's/\\s+/\\t/g' >", paste("RefSeq_annotation/", filename, "_coverage.bed", sep=""), sep=" "))
+system(paste("awk 'NF{NF-=1};1'", paste("RefSeq_annotation/", filename_clinical, "_sort.bed", sep=""), "| uniq | awk '{ print $1\"\t\"$2\"\t\"$3\"\t\"$6\",\"$4\",\"$5\",\"$8\",\"$9\",\"$10}' | sed -r 's/\\s+/\\t/g' >", paste("RefSeq_annotation/", filename_clinical, "_coverage.bed", sep=""), sep=" "))
+
 # index "_sort.bed"
-system(paste("bgzip", paste(filename, "_sort.bed", sep=""), sep=" "))
-system(paste("tabix -b 2 -e 3", paste(filename, "_sort.bed.gz", sep=""), sep=" "))
+#system(paste("bgzip", paste("RefSeq_annotation/", filename, "_sort.bed", sep=""), sep=" "))
+system(paste("bgzip", paste("RefSeq_annotation/", filename_clinical, "_sort.bed", sep=""), sep=" "))
+
+#system(paste("tabix -b 2 -e 3", paste("RefSeq_annotation/", filename, "_sort.bed.gz", sep=""), sep=" "))
+system(paste("tabix -b 2 -e 3", paste("RefSeq_annotation/", filename_clinical, "_sort.bed.gz", sep=""), sep=" "))
