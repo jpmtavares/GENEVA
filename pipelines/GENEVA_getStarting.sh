7f441643ddfd0db06585b3079539a4d1e2eab793
Brígida de Meireles <brigidameireles@gmail.com>
2019-03-29 18:14:13 +0000

    Add scripts to this script for a clean code, add a check to confirm if the samples in the downloaded batch are the same that are in the formulation file (input)

--- pipelines/GENEVA_getStarting.sh
+++ pipelines/GENEVA_getStarting.sh
@@ -1,27 +1,45 @@
 #!/usr/bin/env bash
 # exit when any command fails
 set -e
-#set -o pipefail
+set -o pipefail
 
 #######################################################################################
+#										      #	
 #                                                                                     #
 #     Start the pipeline: download the batch, get samples, genes panel and reports    #
+#										      #
 #                                                                                     #
 #######################################################################################
 
-############################################################
+###################################
+# Description
+###################################
+
+#GENEVA_getStarting - pipeline to download the batch, extract the samples in the batch, preprocessing the pair fastas and associating the gene panel for each samples downloaded. This scripts also organize the samples, moving them to their respective directory and modifyng modifying their name. A log file is created to report all the steps, softawres and files version, also a resume of some results  are reported in the log file. The preprocessing step is done with Fastp (default parameters) and the genes in the gene panel are verified, checked and associated to their respective sample. If all finished with sucess the samples were moved to the Crick_Storage and protected with sudo.
+    #Used scripts: download_extract.sh, samples_preprocessing and get_genepanel.R  
+
+###################################
 #   HELP function
-############################################################
-usage="$(basename "$0") [-h] [-f <formulation file with information: link, plataform and samples [-t <number of process to run>] [-p <password to run the chown> ]
+###################################
+usage="$(basename "$0") [-h] [-f <formulation file with information: link, plataform and samples>] [-p <password to run the chown>] [-t <number of process to run>] 
        -- program that read a formulation file with information regarding a batch and organize the information to start the analysis  --
 where:
     -h    show this help text
     -f    formulation file
-    -t    [default: 5] set number of process to run at the same time - tool: fastp
-    -p    password to run the chown for raw data"
+    -p    password to run the chown for raw data
+    -t    [default: 5] set number of process to run  - tool: fastp"
+
 ##__________ SETUP __________##
+if [ -d "/media/joanatavares/716533eb-f660-4a61-a679-ef610f66feed/" ]; then
+  path="/media/joanatavares/716533eb-f660-4a61-a679-ef610f66feed/"
+else
+  path="/genomedarchive/"
+fi
+
 ## Set number of threads, process
 processes=5
+
+: ${1?"$usage"}
 
 while getopts ':h:f:t:p:' option; do
     case "$option" in
@@ -30,15 +48,15 @@ while getopts ':h:f:t:p:' option; do
        ;;
     f) inputfile=$OPTARG
        ;;
-    t) processes=$OPTARG
-       ;;
     p) pass=$OPTARG
        ;;
-    :) printf "missing argument for -%\n" "$OPTARG" >&2
+    t) processes=$OPTARG
+       ;;
+    :) printf -- "missing argument for -%s\n" "$OPTARG" >&2
        echo "$usage" >&2
        exit 1
        ;;
-   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+   \?) printf -- "illegal option: -%s\n" "$OPTARG" >&2
        echo "$usage" >&2
        exit 1
        ;;
@@ -47,169 +65,81 @@ done
 shift $((OPTIND - 1))
 
 ##__________ SET PRIMARY DIRECTORIES  __________##
-if [ -d "/media/joanatavares/716533eb-f660-4a61-a679-ef610f66feed/" ]; then
-  path="/media/joanatavares/716533eb-f660-4a61-a679-ef610f66feed/"
-else
-  path="/genomedarchive/"
-fi
 path_crick=${path}Crick_storage/
 path_love=${path}Lovelace_decoding/
+path_rosa=${path}Rosalind_resolution/
 
-echo "##__________ Reading the formulation file  __________##"
-#download_samples=$(head -n 1 ${inputfile})
-#file=$(echo ${download_samples##*/}) ##fom var, greedy front trim ##, matches anything *, until the last /
-#plataform=$(cat ${inputfile} | head -n2 | tail -n 1)
-cd ${path_crick}Raw/Downloads/
-
-#echo "Download the samples ..."
-#echo "Start download and extract files ..."
-#wget -c $download_samples -S &> stats.download
-batch_name="$(grep 'Saving to' stats.download | cut -d ':' -f2 | cut -d "." -f1 | sed  "s/ ‘//g" | uniq)"
-extract_name="$(grep 'Saving to' stats.download | cut -d ':' -f2 | sed -e "s/ ‘//" -e "s/’//" | uniq)"
-mv stats.download ${batch_name}.stats.download
-echo "Extract the samples ..."
-tar xvf ${extract_name} --skip-old-files -C ${path_love}Raw/. &> ${path_love}/Raw/${batch_name}.stats.extract
-
-#batch_name="C204HW19020118_20190228_02_s5UxKR"
-plataform="NovoGene"
-echo "Start reading pairs and update or writing logs ..."
-cd ${path_love}Raw/.
-folder_name="$(head -n1 ${batch_name}.stats.extract | cut -d "/" -f1)"
-grep "fq.gz" ${batch_name}.stats.extract | sort > ${folder_name}.pairs.tmp
-grep "fq.gz" ${batch_name}.stats.extract | sort > ${folder_name}.pairs.list_tmp
-path_report=$(grep "eport" ${batch_name}.stats.extract | head -n1)
-mkdir -p ${batch_name}_reports
-cp ${path_love}Raw/${path_report}* ${batch_name}_reports/.
-
-while read -r first_pair; do
-  read -r second_pair
-  var=$(python ${path_love}bin/get_sample_name.py -f ${first_pair} -r ${second_pair} -p ${plataform} -b ${batch_name})
-  if [[ $var =~ ^ERROR ]]; then
-    echo " ----- !!!!An error!!!! -----"
-    echo ${var}
-    sed -i '$ d' ${path_crick}Source_control/sample_batch.info
-    echo "From the batch ${batch_name} need to be checked!"
-    echo " ----- !!!!An error!!!! -----"
-    continue
-  fi
-  IFS="," read -ra elements <<< "$var"
-  sample_name="${elements[0]}"
-  out_f="${elements[1]}"
-  out_r="${elements[2]}"
-  pair1="${elements[3]}"
-  pair2="${elements[4]}"
-  raw_name="${elements[5]}"
-  if [[ $sample_name == "Empty" ]]; then
-     echo "The sample name has a format different from usual, please insert the sample name in the following format (NNNNN-LL):"
-     read -p  "--> " </dev/tty
-     sample_name="$REPLY"
-     out_f=${sample_name}"_1.fastp.fq.gz"
-     out_r=${sample_name}"_2.fastp.fq.gz"
-  fi
-  log_name="log_"$sample_name
-  mv log_ $log_name
-  echo "                        ==> First pair: $out_f" >> $log_name
-  echo "                        ==> First pair: $out_r" >> $log_name
-  mv $log_name ${path_love}log/.
-  var=$(echo ${first_pair} | sed -e "s/\/${pair1}//g")
-  echo "Pairs are correct, so the files will be moved ..."
-  directory=$(dirname ${first_pair})
-  mkdir -p ${sample_name}
-  cp ${path_love}Raw/${directory}/* ${sample_name}/.
-  rm -r ${path_love}Raw/${directory}
-
-  echo "All organized, starting the fastp tool ... for ${sample_name}"
-  mkdir -p ${path_love}Analysis/${sample_name}
-  mkdir -p ${path_love}Analysis/${sample_name}/fastp
-  cd ${path_love}Analysis/${sample_name}/fastp
-  log_fastp="fastp_log_"${sample_name}
-  echo "[" "$(date '+%Y-%m-%d %H:%M:%S' )" "]" "Starting the preprocessing with FASTP tool:" >> ${path_love}log/${log_name}
-  fastp -i ${path_love}Raw/$sample_name/${pair1} -o ${out_f} -I ${path_love}Raw/$sample_name/${pair2} -O ${out_r} -w ${processes} -z 2 &>> ${log_fastp}
-  echo "Fastp tool finished, check if an error occur!"
-  if tail ${log_fastp} | grep "time used"; then
-    read1_b=$(grep -A1 "Read1 before filtering" $log_fastp | grep "total" | cut -d ":" -f2 | tr -d '[:space:]')
-    read1_a=$(grep -A1 "Read1 after filtering" $log_fastp | grep "total" | cut -d ":" -f2 | tr -d '[:space:]')
-    read2_b=$(grep -A1 "Read2 before filtering" $log_fastp | grep "total" | cut -d ":" -f2 | tr -d '[:space:]')
-    read2_a=$(grep -A1 "Read2 aftering filtering" $log_fastp | grep "total" | cut -d ":" -f2 | tr -d '[:space:]')
-    version=$(grep "fastp v" $log_fastp | cut -d "," -f1)
-    pair1_perc=$(echo "scale=4; $read1_a/$read1_b*100" | bc)
-    pair2_perc=$(echo "scale=4; $read2_a/$read2_b*100" | bc)
-    echo "[" "$(date '+%Y-%m-%d %H:%M:%S' )" "]" "Result from $version:" >> ${path_love}log/${log_name}
-    echo "                        Total numer of reads before preprocessing:" >> ${path_love}log/${log_name}
-    echo "                        ==> First pair: $read1_b reads" >> ${path_love}log/${log_name}
-    echo "                        ==> Second pair: $read2_b reads" >> ${path_love}log/${log_name}
-    echo "                        Total numer of reads after preprocessing:" >> ${path_love}log/${log_name}
-    echo "                        ==> First pair: $read1_a reads($pair1_perc %)" >> ${path_love}log/${log_name}
-    echo "                        ==> Second pair: $read2_a reads ($pair2_perc %)" >> ${path_love}log/${log_name}
-  else
-    echo " ----- !!!!An error!!!! -----"
-    echo "Some error occurs when running the fastp for the sample ${sample_name}, this need to be checked!"
-    echo " ----- !!!!An error!!!! -----"
-    mv ${log_fastp} ${path_love}log/.
-    rm -r ${path_love}Analysis/${sample_name}/
-    continue
-  fi
-  cd ${path_love}Raw/.
-  echo "${sample_name}"
-
-  mv -n ${sample_name} ${path_crick}Raw/2019/.
-  if [ -d "${path_love}Analysis/${sample_name}" ]; then
-    sed -i "s/.*${sample_name}//g" ${folder_name}.pairs.list_tmp
-    sed -i "/^[[:space:]]*$/d" ${folder_name}.pairs.list_tmp
-  fi
-done < ${folder_name}.pairs.tmp
-
-echo "Creating the gene panel for each sample ..."
-Rscript ${path_love}bin/get_genepanel.R --fromana=="${inputfile}"
-cd ${path_love}Raw/.
-if [ -s ${folder_name}.pairs.list_tmp ]; then
-  cd ${path_crick}Source_control/gene_panel 
-  if find . -name '*pending' -printf 1 -quit | grep -q 1; then 
-    ls *genepanel.pending >> pending_file.tmp
-    while IFS='' read -r line || [[-n "$line" ]]; do
-      sample_id=$(echo ${line} | cut -d "_" -f1)
-      mv ${line} ${path_lovelace}Raw/.
-      mv ${path_crick}Raw/2019/${sample_id} ${path_lovelace}Raw/.
-    done < pending_file.tmp
-    rm pending_file.tmp 
-  fi
-  ls *genepanel.txt >> genepanels_file.tmp
-  while IFS='' read -r line || [[ -n "$line" ]]; do
-    sample_id=$(echo ${line} | cut -d "_" -f1)   
-    mv ${line} ${path_crick}Raw/2019/${sample_id}/.
-  done < genepanels_file.tmp
-  rm genepanels_file.tmp    
-  mv ${path_love}Raw/${batch_name}_reports/* ${path_crick}Raw/2019/Reports/.
-  rm -r ${path_love}Raw/${folder_name}*tmp
-  rm -r ${path_love}Raw/${batch_name}*
+##_______________CHECK IF ALL IS READY TO START THE PREPROCESSING ANALYSIS_______________##
+if [ -e ${path_crick}Source_control/gene_panel/${inputfile} ]; then 
+  printf -- "Starting the analysis for this batch ...\n"
 else
-  if find . -name '*pending' -printf 1 -quit | grep -q 1; then
-    ls *genepanel.pending >> pending_file.tmp
-    while IFS='' read -r line || [[ -n "$line" ]]; do
-      sample_id=$(echo ${line} | cut -d "_" -f1)
-      if [ -d "${path_crick}Raw/2019/${sample_id}" ]; then
-        mv ${path_crick}Raw/2019/${sample_id} ${path_lovelace}Raw/.
-      fi 
-      mv ${line} ${path_lovelace}Raw/.
-    done < pending_file.tmp
-    rm pending_file.tmp
-  else
-    ls *genepanel.txt >> genepanels_file.tmp
-    while IFS='' read -r line || [[ -n "$line" ]]; do
-      sample_id=$(echo ${line} | cut -d "_" -f1)
-      if [ -d "${path_crick}Raw/2019/${sample_id}" ]; then
-        mv ${line} ${path_crick}Raw/2019/${sample_id}/.
-      else
-        mv ${line} ${path_lovelace}Raw/.
-      fi
-    done < genepanels_file.tmp
-    rm genepanels_file.tmp
-  fi
+  printf -- '\033[31m ERROR: Input file is missing \033[0m\n';
+  exit 1
 fi
-cd ${path_love}Raw/
-rm -r ${folder_name}
-cd ${path_crick}Raw/2019/.
+_=$(command -v ${path_love}bin/get_genepanel.R);
+if [ "$?" != "0" ]; then
+  printf -- '\033[31m ERROR: Missing the script: get_genepanel.R, check this error! \033[0m\n';
+  exit 1
+fi
+_=$(command -v ${path_love}bin/get_sample_name.py);
+if [ "$?" != "0" ]; then
+  printf -- '\033[31m ERROR: Missing the script: get_sample_name.py, check this error! \033[0m\n';
+  exit 1
+fi
+_=$(command -v ${path_love}bin/download_extract.sh);
+if [ "$?" != "0" ]; then
+  printf -- '\033[31m ERROR: Missing the script: download_extract.sh, check this error! \033[0m\n';
+  exit 1
+fi
+
+##__________ STARTING THE DOWNLOAD AND EXTRACTION__________##
+${path_love}bin/download_extract.sh -f ${inputfile}
+line=$(head -n 1 ${path_love}Raw/download.tmp)
+IFS=' ' read -ra elements <<< "${line}"
+pairs_file="${elements[0]}"
+batch_name="${elements[1]}"
+plataform="${elements[2]}"
+rm ${path_love}Raw/download.tmp
+##__________ STARTING THE PREPROCESSING__________##
+${path_love}bin/do_preprocessing.sh -f ${pairs_file} -b ${batch_name} -p ${plataform}
+mv ${path_crick}Source_control/${batch_name}.batch_samplesList ${path_crick}Source_control/${inputfile}.batch_samplesList
+more  ${path_crick}Source_control/${inputfile}.batch_samplesList
+
+##__________ GETTING THE GENES OF GENE PANEL ___________##
+echo "[" "$(date '+%Y-%m-%d %H:%M:%S' )" "]" "Getting the genes in gene panel for each sample in the ${inputfile}"
+${path_love}bin/get_genepanel.R --fromana=="${inputfile}"
+
+##__________ PROTECTING THE RAW DATA AND CHECK IF AN ERROR OCCURRED __________##
+cd ${path_crick}Source_control/
+tail -n +3 ${path_crick}Source_control/gene_panel/${inputfile} | cut -f1 > ${path_crick}Source_control/${inputfile}.genepanel_samplesList
+while IFS='\n' read -r line || [[ -n "$line" ]]; do
+  if ! grep -q "Starting Gene Panel" ${path_love}log/log_${line}; then 
+    printf -- "\033[33m WARNING: ${line} don't have an gene panel associated! Check. \033[0m\n";
+  else
+    if grep -q "ERROR" ${path_love}log/log_${line}; then 
+      errortype=$(grep -A1 "ERROR" ${path_love}log/log_${line} | tail -n1 | sed -zE 's/[[:space:]]*([[:space:]])/\1/g') 
+      printf -- "\033[31m ERROR: ${line} \033[0m\n"
+      printf -- "\033[31m       ${errortype} \033[0m\n"
+    fi
+  fi  
+if [ -f ${path_love}to_do/${line}_genepanel.pending ]; then 
+  printf -- "\033[33m WARNING: ${line} have genes pending! Go to to_do and check. \033[0m\n"; 
+fi
+done < *${inputfile}*batch_samplesList*
+
+if grep -q -v -F -x -f ${inputfile}.batch_samplesList ${inputfile}.genepanel_samplesList; then
+  samplesnobatch=$(grep -v -F -x -f ${inputfile}.batch_samplesList ${inputfile}.genepanel_samplesList | sed -zE 's/\n/    /g')
+  printf -- "\033[31m ERROR: Sample(s) in ${inputfile} but not in batch. \033[0m\n"
+  printf -- "\033[31m        ${samplesnobatch} \033[0m\n"
+else
+  printf -- "\033[32m SUCCESS: All samples have a gene panel associated \033[0m\n";
+fi
+rm ${path_crick}Source_control/${inputfile}.genepanel_samplesList
+rm ${path_crick}Source_control/${inputfile}.batch_samplesList
+rm ${path_crick}Source_control/gene_panel/${inputfile}
+
+cd ${path_crick}Raw/2019/
 echo "${pass}" | sudo -S chown -R root:root *-*
 
-echo "##__________ Extract and Preprocessing done! __________##"
-echo "##__________  Good luck for the next steps!  __________##"
+printf -- "\n-----------------------------------------------------------------\n"
+printf -- "Finished this part of the analysis! Ready to start the bcbio part\n"
