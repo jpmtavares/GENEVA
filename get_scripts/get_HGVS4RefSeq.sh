2ce420a12c0f004e8c22fc09394ec2b6c69fb0e7
Joana Tavares <jpmtav@gmail.com>
2019-04-02 18:01:08 +0100

    Update get_HGVS4RefSeq.sh
    writes output message and erros to log file; checks if some chromosome was already runned and skips search for those; merge all VEP output results for each chromosome in a final separate step, before compressing chromosome files

--- get_scripts/get_HGVS4RefSeq.sh
+++ get_scripts/get_HGVS4RefSeq.sh
@@ -60,13 +60,16 @@ while getopts ':h' option; do
 done
 shift $((OPTIND - 1))
 
+# Output to log file
+exec >  >(tee -ia ${MENDEL}log_grch37.clin.hgvs_dbsnp.txt)
+exec 2> >(tee -ia ${MENDEL}log_grch37.clin.hgvs_dbsnp.txt >&2)
 
 echo "_______________________________________________________________"
 echo "STEP1: Getting clinical RefSeq mRNA transcripts from $(basename ${RefSeq})"
 echo "_______________________________________________________________"
 echo
 #look for column called "refSeq_mRNA_noVersion" and print it to file 
-awk -F'\t' -v c="refSeq_mRNA_noVersion" 'NR==1{for (i=1; i<=NF; i++) if ($i==c){p=i; break}; next} {print $p}' ${RefSeq} | sort | uniq >> ${MENDEL}nm_transcripts.txt
+awk -F'\t' -v c="refSeq_mRNA_noVersion" 'NR==1{for (i=1; i<=NF; i++) if ($i==c){p=i; break}; next} {print $p}' ${RefSeq} | sort | uniq > ${MENDEL}nm_transcripts.txt
 
 echo "Reading $(basename ${RefSeq}) and get unique transcripts in 'refSeq_mRNA_noVersion' column"
 
@@ -82,6 +85,15 @@ echo
 
 for CHRformat in ${chromosomes}chr*.vep*.cache*.homo37.*; do
   chrname=$(basename ${CHRformat})
+
+  # check if a chromosome was already runned
+  alreadydone=$(ls ${MENDEL}grch37.clin.hgvs_dbsnp.${chrname%%.*}.vcf 2>/dev/null)
+  if [[ ! -z $alreadydone ]]; then
+    echo "[" "$(date '+%Y-%m-%d %H:%M:%S' )" "]" "HGVS nomenclature already done for ${chrname%%.*}"
+    echo
+    continue
+  fi
+
   echo "[" "$(date '+%Y-%m-%d %H:%M:%S' )" "]" "Get HGVS nomenclature for ${chrname%%.*}"
 
   # Uncompressed chromosome files if necessary
@@ -100,7 +112,7 @@ for CHRformat in ${chromosomes}chr*.vep*.cache*.homo37.*; do
   fgrep -f ${MENDEL}nm_transcripts.txt ${chr} > ${MENDEL}vepout_${chrname%%.*}.tmp.txt
 
   # Split VEP output
-  split --line-bytes=500MB ${MENDEL}vepout_${chrname%%.*}.tmp.txt ${MENDEL}vepout_${chrname%%.*}.tmp.split
+  split --line-bytes=100MB ${MENDEL}vepout_${chrname%%.*}.tmp.txt ${MENDEL}vepout_${chrname%%.*}.tmp.split
 
   # Run get_HGVSnomenclature.py
   echo "                       " "Run get_HGVSnomenclature.py"
@@ -116,28 +128,50 @@ for CHRformat in ${chromosomes}chr*.vep*.cache*.homo37.*; do
     more $file >> ${MENDEL}grch37.clin.hgvs_dbsnp.${chrname%%.*}.warnings.vcf; 
   done
 
-  sort -k1,1 -k2,2n -T . ${MENDEL}grch37.clin.hgvs_dbsnp.${chrname%%.*}.tmp.txt > ${MENDEL}grch37.clin.hgvs_dbsnp.${chrname%%.*}.vcf
-
-  # Zip and Index files
-  echo "                       " "Zip and Index files"
-  bgzip ${MENDEL}grch37.clin.hgvs_dbsnp.${chrname%%.*}.vcf
-  bgzip ${MENDEL}grch37.clin.hgvs_dbsnp.${chrname%%.*}.warnings.vcf
-  tabix -p vcf ${MENDEL}grch37.clin.hgvs_dbsnp.${chrname%%.*}.vcf.gz
+  sort -k1,1 -k2,2n -T ${MENDEL} ${MENDEL}grch37.clin.hgvs_dbsnp.${chrname%%.*}.tmp.txt > ${MENDEL}grch37.clin.hgvs_dbsnp.${chrname%%.*}.vcf
 
   #Remove files
   rm ${MENDEL}*tmp*
   
   echo "                       " "$(basename ${chr}) is Done."
-  echo
 
 done
+
 echo "_______________________________________________________________"
 echo "STEP2 Done."
 echo "_______________________________________________________________"
 echo
 
 echo "_______________________________________________________________"
-echo "STEP3: Compress VEP outputs..."
+echo "STEP3: Merge all chromosomes into final file"
+echo "_______________________________________________________________"
+echo
+# Merge all chromosomes into final file
+echo "                       " "Merge all chromosomes"
+cat ${MENDEL}grch37.clin.hgvs_dbsnp.chr*.warnings.vcf > ${MENDEL}grch37.clin.hgvs_dbsnp.warnings.vcf
+rm ${MENDEL}grch37.clin.hgvs_dbsnp.chr*.warnings.vcf
+cat ${MENDEL}grch37.clin.hgvs_dbsnp.chr*.vcf > ${MENDEL}grch37.clin.hgvs_dbsnp.vcf
+rm ${MENDEL}grch37.clin.hgvs_dbsnp.chr*.vcf
+
+# Sort
+echo "                       " "Sort, BGZip and Index final file"
+sort -k1,1 -k2,2n -T ${MENDEL} ${MENDEL}grch37.clin.hgvs_dbsnp.vcf > ${MENDEL}grch37.clin.hgvs_dbsnp.sort.vcf
+mv ${MENDEL}grch37.clin.hgvs_dbsnp.sort.vcf ${MENDEL}grch37.clin.hgvs_dbsnp.vcf
+
+# Add header
+sed -i "1i\#Chr\tPos\tRef\tAlt\trefSeq_mRNA\tHGVSc\trefSeq_protein\tHGVSp\trs_id" ${MENDEL}grch37.clin.hgvs_dbsnp.vcf
+
+# BGZip and Tabix
+bgzip ${MENDEL}grch37.clin.hgvs_dbsnp.vcf
+bgzip ${MENDEL}grch37.clin.hgvs_dbsnp.warnings.vcf
+tabix -p vcf ${MENDEL}grch37.clin.hgvs_dbsnp.vcf.gz
+
+echo "_______________________________________________________________"
+echo "STEP3 Done."
+echo "_______________________________________________________________"
+
+echo "_______________________________________________________________"
+echo "STEP4: Compress VEP outputs..."
 echo "_______________________________________________________________"
 echo
 
@@ -149,7 +183,7 @@ echo
 done
 
 echo "_______________________________________________________________"
-echo "STEP3 Done."
+echo "STEP4 Done."
 echo "_______________________________________________________________"
 
 #Remove files
